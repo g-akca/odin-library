a3642b7ba679360d2902dd4147c42fd8f77704cd
g-akca <gok.akca66@gmail.com>
2026-03-05 02:57:28 +0300

    Add function to display books in the library

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "Title",
+        author: "Author Person",
+        pages: "150",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod. Ut enim exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        read: false
+    }
+];
+const booksSection = document.getElementById("books-section");
 
 function Book(title, author, pages, description, read) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.description = description;
-  this.read = read;
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.description = description;
+    this.read = read;
 }
 
 Book.prototype.toggleRead = function () {
-  this.read = !this.read;
+    this.read = !this.read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
+function addBookToLibrary(title, author, pages, description, read) {
+    const newBook = new Book(title, author, pages, description, read);
+    myLibrary.push(newBook);
 }
+
+function displayBooks() {
+    myLibrary.forEach(book => {
+        const newElement = document.createElement("div");
+        newElement.classList.add("book-card");
+        newElement.innerHTML = `
+            <div class="book-header">
+                <div>
+                    <p class="book-title">${book.title}</p>
+                    <p class="book-author">${book.author}</p>
+                </div>
+                <p class="book-pages">${book.pages} pages</p>
+            </div>
+            <div class="book-main">
+                <p>${book.description}</p>
+            </div>
+            <div class="book-footer">
+                <button type="button" class="not-read">NOT READ</button>
+                <button type="button" class="delete">DELETE</button>
+            </div>
+        `;
+
+        booksSection.appendChild(newElement);
+    });
+}
+
+displayBooks();
